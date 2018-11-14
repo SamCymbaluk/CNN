0c2086109e1d000d206f3655c0d733fd04d577c5
SamCymbaluk <samuelcymbaluk@gmail.com>
2018-11-14 10:13:16 -0500

    Add examples at the end of mnist demo

--- mnist_demo.c
+++ mnist_demo.c
@@ -15,15 +15,29 @@ int main() {
                           "DATASETS/mnist/test-images",
                           "DATASETS/mnist/test-labels");
 
-    mnist.trainElements = 10000;
-    mnist.testElements = 50;
+    // mnist.trainElements = 10000;
+    // mnist.testElements = 50;
 
     // Initialize neural network
-    unsigned int nnShape[] = {784, 32, 10};
-    NeuralNet* nn = newNeuralNet(3, nnShape, MeanSquaredError);
+    unsigned int nnShape[] = {784, 16, 16, 10};
+    NeuralNet* nn = newNeuralNet(4, nnShape, MeanSquaredError);
     randInit(nn);
     nn->train = true;
 
-    train(nn, SGD, mnist, 20, 1, 0.1, printEpochCallback);
+    train(nn, SGD, mnist, 10, 1, 0.1, printEpochCallback);
+
+    for (size_t i = 0; i < 10; i++) {
+        printf("\n");
+        Datum datum = mnist.getTestElement(i);
+        printMnistDatum(datum);
+
+        copyTensor(datum.x, nn->input);
+        forwardPass(nn);
+
+        printf("Network prediction: %zu\n", argmax(nn->output));
+        printf("Confidence: %f%%\n", 100*nn->output->data[argmax(nn->output)]);
+        printf("Raw output: ");
+        printTensor(nn->output, false);
+    }
 }
 
